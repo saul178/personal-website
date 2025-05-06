f83be803c542a0b58430ab986fa15de4648ad9ab
Saul Gonzalez <gonzalezsaul31@gmail.com>
2025-05-06 16:00:09 -0600

    deleted generated stuff from the cli command

--- backend/main.go
+++ backend/main.go
@@ -1,3 +1,17 @@
 package main
 
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
 // gin backend will be here
+
+func main() {
+	r := gin.Default()
+
+	r.GET("/", func(c *gin.Context) {
+		c.HTML(http.StatusOK, "index.html", gin.H)
+	})
+}
