55c3352cbd292fa5411b10c73382899fbc99a254
Saul Gonzalez <gonzalezsaul31@gmail.com>
2025-05-19 16:27:23 -0600

    feat: separated dark mode into it's own component, will move links to their own

--- frontend/ui/src/components/Navbar.vue
+++ frontend/ui/src/components/Navbar.vue
@@ -26,7 +26,7 @@
         </button>
       </div>
 
-      <!-- links -->
+      <!-- Desktop view links -->
       <div class="hidden md:flex items-center space-x-4">
         <RouterLink to="/" class="py-2 px-3 block dark:hover:text-accent transition-colors"
           active-class="dark:text-accent" exact>
@@ -48,27 +48,12 @@
           Personal
         </RouterLink>
       </div>
-      <!-- dark and light mode icon -->
-      <button @click="toggleDark()" class="hidden md:flex py-2 px-3 dark:hover:text-accent transition-colors">
-        <span v-if="isDark">
-          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
-            stroke="currentColor" class="size-6">
-            <path stroke-linecap="round" stroke-linejoin="round"
-              d="M21.752 15.002A9.72 9.72 0 0 1 18 15.75c-5.385 0-9.75-4.365-9.75-9.75 0-1.33.266-2.597.748-3.752A9.753 9.753 0 0 0 3 11.25C3 16.635 7.365 21 12.75 21a9.753 9.753 0 0 0 9.002-5.998Z" />
-          </svg>
-        </span>
-        <span v-else>
-          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
-            stroke="currentColor" class="size-6">
-            <path stroke-linecap="round" stroke-linejoin="round"
-              d="M12 3v2.25m6.364.386-1.591 1.591M21 12h-2.25m-.386 6.364-1.591-1.591M12 18.75V21m-4.773-4.227-1.591 1.591M5.25 12H3m4.227-4.773L5.636 5.636M15.75 12a3.75 3.75 0 1 1-7.5 0 3.75 3.75 0 0 1 7.5 0Z" />
-          </svg>
-        </span>
-      </button>
+      <div class="hidden md:flex">
+        <ThemeToggle />
+      </div>
     </div>
 
     <!-- mobile stuff need to make it dry -->
-
     <div v-if="isMobileMenuOpen" class="md:hidden flex flex-col px-4 pb-4 space-y-2
       dark:bg-foreground">
       <RouterLink to="/" class="py-2 px-3 block dark:hover:text-accent transition-colors"
@@ -90,23 +75,7 @@
         active-class="dark:text-accent" exact>
         Personal
       </RouterLink>
-
-      <button @click="toggleDark()" class="py-2 px-3 dark:hover:text-accent transition-colors">
-        <span v-if="isDark">
-          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
-            stroke="currentColor" class="size-6">
-            <path stroke-linecap="round" stroke-linejoin="round"
-              d="M21.752 15.002A9.72 9.72 0 0 1 18 15.75c-5.385 0-9.75-4.365-9.75-9.75 0-1.33.266-2.597.748-3.752A9.753 9.753 0 0 0 3 11.25C3 16.635 7.365 21 12.75 21a9.753 9.753 0 0 0 9.002-5.998Z" />
-          </svg>
-        </span>
-        <span v-else>
-          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
-            stroke="currentColor" class="size-6">
-            <path stroke-linecap="round" stroke-linejoin="round"
-              d="M12 3v2.25m6.364.386-1.591 1.591M21 12h-2.25m-.386 6.364-1.591-1.591M12 18.75V21m-4.773-4.227-1.591 1.591M5.25 12H3m4.227-4.773L5.636 5.636M15.75 12a3.75 3.75 0 1 1-7.5 0 3.75 3.75 0 0 1 7.5 0Z" />
-          </svg>
-        </span>
-      </button>
+        <ThemeToggle mobile="true" />
     </div>
   </nav>
 </template>
@@ -114,7 +83,7 @@
 <script setup>
 import { ref } from 'vue';
 import { RouterLink } from 'vue-router'
-import { useDark, useToggle } from '@vueuse/core'
+import ThemeToggle from './ThemeToggle.vue';
 
 const isMobileMenuOpen = ref(false)
 
@@ -122,8 +91,6 @@ const toggleMobileMenu = () => {
   isMobileMenuOpen.value = !isMobileMenuOpen.value
 }
 
-const isDark = useDark()
-const toggleDark = useToggle(isDark)
 </script>
 
 <style scoped></style>
