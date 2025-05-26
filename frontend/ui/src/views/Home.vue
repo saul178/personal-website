e7e153f63e19539cfabba8b3b670d84b7a27dc91
Saul Gonzalez <gonzalezsaul31@gmail.com>
2025-05-26 16:23:06 -0600

    wip: need to find a plugin that will fold the long svg snippet or maybe just switch to getting the image?

--- frontend/ui/src/views/Home.vue
+++ frontend/ui/src/views/Home.vue
@@ -80,7 +80,8 @@
       </div>
 
       <!-- tools and frameworks -->
-      <h1 class="text-lg font-medium mt-4 pt-2 dark:text-primary"> Proficient with these tools and frameworks </h1>
+      <h1 class="text-lg font-medium mt-4 pt-2 dark:text-primary"> Proficient with these
+        technologies </h1>
       <div class="flex items-start gap-4 mt-3">
         <div class="hidden md:flex flex-col items-center pt-12 px-3">
           <font-awesome-icon :icon="['fas', 'screwdriver-wrench']" class="dark:text-primary
@@ -88,13 +89,10 @@
         </div>
         <div class="grid grid-cols-2 md:grid-cols-4 gap-4 mt-4 w-full">
           <div class="dark:bg-background p-3 rounded-lg shadow-md shadow-satin-sheen-gold-500 text-center">
-            <font-awesome-icon :icon="['fab', 'vuejs']" class="dark:text-primary text-5xl" alt="Vuejs" />
-            <span class="dark:text-primary block mt-1"> Vuejs </span>
-          </div>
-          <div class="dark:bg-background p-3 rounded-lg shadow-md shadow-satin-sheen-gold-500 text-center">
             <font-awesome-icon :icon="['fab', 'docker']" class="dark:text-primary text-5xl" alt="docker" />
             <span class="dark:text-primary block mt-1"> Docker </span>
           </div>
+
           <div class="dark:bg-background p-3 rounded-lg shadow-md shadow-satin-sheen-gold-500 text-center">
             <font-awesome-icon :icon="['fab', 'git-alt']" class="dark:text-primary text-5xl" alt="Git" />
             <span class="dark:text-primary block mt-1"> Git </span>
@@ -102,6 +100,51 @@
           <div class="dark:bg-background p-3 rounded-lg shadow-md shadow-satin-sheen-gold-500 text-center">
             <font-awesome-icon :icon="['fab', 'github']" class="dark:text-primary text-5xl" alt="Github" />
             <span class="dark:text-primary block mt-1"> Github </span>
+          </div>
+        </div>
+      </div>
+
+      <!-- framework grid -->
+      <h1 class="text-lg font-medium mt-4 pt-2 dark:text-primary"> Proficient with these frameworks </h1>
+      <div class="flex items-start gap-4 mt-3">
+        <div class="hidden md:flex flex-col items-center pt-12 px-3">
+          <font-awesome-icon :icon="['fas', 'layer-group']" class="dark:text-primary text-4xl" alt="Frameworks" />
+        </div>
+        <div class="grid grid-cols-2 md:grid-cols-4 gap-4 mt-4 w-full">
+          <div class="dark:bg-background p-3 rounded-lg shadow-md shadow-satin-sheen-gold-500 text-center">
+            <font-awesome-icon :icon="['fab', 'vuejs']" class="dark:text-primary text-5xl" alt="Vuejs" />
+            <span class="dark:text-primary block mt-1"> Vuejs </span>
+          </div>
+          <div class="dark:bg-background p-3 rounded-lg shadow-md shadow-satin-sheen-gold-500 text-center">
+            <svg viewBox="0 0 15 15" fill="none" class="w-12 h-12 mx-auto dark:text-primary"
+              xmlns="http://www.w3.org/2000/svg">
+              <g id="SVGRepo_bgCarrier" stroke-width="0"></g>
+              <g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g>
+              <g id="SVGRepo_iconCarrier">
+                <path fill-rule="evenodd" clip-rule="evenodd"
+                  d="M7.5 3C5.63333 3 4.46667 4 4 5.99999C4.7 4.99999 5.51667 4.625 6.45 4.87499C6.98252 5.01763 7.36314 5.43155 7.78443 5.88974C8.47074 6.63613 9.26506 7.49999 11 7.49999C12.8667 7.49999 14.0333 6.49999 14.5 4.5C13.8 5.49999 12.9833 5.87499 12.05 5.62499C11.5175 5.48235 11.1369 5.06844 10.7156 4.61025C10.0293 3.86386 9.23494 3 7.5 3ZM4 7.49999C2.13333 7.49999 0.966667 8.49998 0.5 10.5C1.2 9.49998 2.01667 9.12498 2.95 9.37498C3.48252 9.51762 3.86314 9.93154 4.28443 10.3897C4.97074 11.1361 5.76506 12 7.5 12C9.36667 12 10.5333 11 11 8.99998C10.3 9.99998 9.48333 10.375 8.55 10.125C8.01748 9.98234 7.63686 9.56843 7.21557 9.11023C6.52926 8.36385 5.73494 7.49999 4 7.49999Z"
+                  stroke="currentColor" stroke-linejoin="round"></path>
+              </g>
+            </svg>
+            <span class="dark:text-primary block mt-1"> Tailwindcss </span>
+          </div>
+          <div class="dark:bg-background p-3 rounded-lg shadow-md shadow-satin-sheen-gold-500 text-center">
+            <svg fill="#000000" viewBox="0 0 32 32" class="dark:text-primary w-12 h-12 mx-auto"
+              xmlns="http://www.w3.org/2000/svg">
+              <g id="SVGRepo_bgCarrier" stroke-width="0"></g>
+              <g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g>
+              <g id="SVGRepo_iconCarrier">
+                <path d="M14.859 0h5.234v24.219c-2.682 0.51-4.656 0.714-6.797 0.714-6.385
+                  0-9.714-2.885-9.714-8.427 0-5.333 3.531-8.797 9-8.797 0.849 0 1.495 0.068 2.276
+                  0.271zM14.859 12.193c-0.568-0.193-1.167-0.286-1.766-0.276-2.651 0-4.177 1.63-4.177
+                  4.49 0 2.786 1.458 4.313 4.146 4.313 0.578 0 1.052-0.031
+                  1.797-0.135v-8.396zM28.417 8.078v12.13c0 4.177-0.302 6.188-1.219 7.917-0.849
+                  1.667-1.974 2.719-4.281 3.875l-4.859-2.313c2.307-1.089 3.432-2.036 4.146-3.5
+                  0.745-1.495 0.984-3.229 0.984-7.781v-10.328zM23.188 0.026h5.229v5.37h-5.229z" stroke="currentColor">
+                </path>
+              </g>
+            </svg>
+            <span class="dark:text-primary block mt-1"> Django </span>
           </div>
         </div>
       </div>
