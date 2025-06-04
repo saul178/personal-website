1cbaaa23c26c6ddffb8f4bbbd6f69961fe32a4c1
Saul Gonzalez <gonzalezsaul31@gmail.com>
2025-06-04 14:49:16 -0600

    wip: progressing

--- frontend/ui/src/views/Projects.vue
+++ frontend/ui/src/views/Projects.vue
@@ -1,1 +1,22 @@
-<template></template>
+<!-- what to show from the github api
+name or full name not sure if this is for the repo or user
+description box of the repo
+created at, updated at or pushed at?
+commits??
+url hyperlink?
+
+at the top or bottom have total commits?
+
+-->
+
+<template>
+  <div class="flex flex-row">
+    <div class="flex flex-row w-full justify-center">
+      <div class="dark:bg-foreground rounded-2xl shadow-lg p-6 w-full max-w-2xl text-center">
+      </div>
+
+    </div>
+
+  </div>
+
+</template>
