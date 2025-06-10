e891a586b8385a8f7e7959293fcd25fbf903e161
leviathan <gonzalezsaul31@gmail.com>
2025-06-09 19:27:53 -0600

    wip: need to rethink how i want to show commits in the projects page, redo light theme

--- frontend/ui/src/views/Projects.vue
+++ frontend/ui/src/views/Projects.vue
@@ -48,27 +48,83 @@ at the top or bottom have total commits?
         <div class="md:flex flex-row hidden mt-6 border-t-2 border-royal-blue-100">
           <h3 class="dark:text-secondary font-semibold mt-2">Commit History Example</h3>
         </div>
-        <div class="flex flex-row w-full mt-4">
-          <img class="w-full" src="https://ghchart.rshah.org/saul178" alt="Github Contributations Chart">
+        <div class="mt-4">
+          <span class="dark:text-secondary">Latest commit here!</span>
         </div>
       </div>
 
       <!-- Project Card 2 -->
       <div class="group hover:-translate-y-1 bg-foreground backdrop-blur-sm p-6 rounded-2xl border
         border-accent/20 transition-all duration-300 shadow-lg hover:shadow-accent/20">
-        <h3 class="text-xl font-semibold dark:text-primary">Another Project</h3>
-        <p class="mt-2 dark:text-disable">
-          Another description here.
-        </p>
-
+        <div>
+          <h3 class="text-xl font-semibold dark:text-primary">Project 2 Title Here</h3>
+        </div>
+        <div>
+          <p class="mt-2 dark:text-secondary"> <!-- this need to change for lightmode -->
+            Another PROJECT DESCRIPTION HERE She's asked the question so many times that she barely listened to the
+            answers
+            anymore. The answers were
+            always the same. Well, not exactly the same, but the same in a general sense. A more accurate description
+            was the answers never surprised her. So, she asked for the 10,000th time, "What's your favorite animal?" But
+            this time was different. When she heard the young boy's answer, she wondered if she had heard him correctly.
+          </p>
+        </div>
+        <!-- tag rows (this will probably be a vue component) -->
         <div class="flex flex-row flex-wrap gap-4 mt-4">
           <span class="dark:text-primary px-2 py-1 rounded-full text-sm border-2 border-accent">Tag 1</span>
           <span class="dark:text-primary px-2 py-1 rounded-full text-sm border-2 border-accent">Tag 2</span>
           <span class="dark:text-primary px-2 py-1 rounded-full text-sm border-2 border-accent">Tag 3</span>
         </div>
+
+        <div class="md:flex flex-row hidden mt-6 border-t-2 border-royal-blue-100">
+          <h3 class="dark:text-secondary font-semibold mt-2">Commit History Example</h3>
+        </div>
+        <div class="mt-4">
+          <span class="dark:text-secondary">Latest commit here!</span>
+        </div>
       </div>
 
+      <!-- Project Card 3 -->
+      <div class="group hover:-translate-y-1 bg-foreground backdrop-blur-sm p-6 rounded-2xl border
+        border-accent/20 transition-all duration-300 shadow-lg hover:shadow-accent/20">
+        <div>
+          <h3 class="text-xl font-semibold dark:text-primary">Project 3 Title Here</h3>
+        </div>
+        <div>
+          <p class="mt-2 dark:text-secondary"> <!-- this need to change for lightmode -->
+            PROJECT DESCRIPTION HERE She's asked the question so many times that she barely listened to the answers
+            anymore. The answers were
+            always the same. Well, not exactly the same, but the same in a general sense. A more accurate description
+            was the answers never surprised her. So, she asked for the 10,000th time, "What's your favorite animal?" But
+            this time was different. When she heard the young boy's answer, she wondered if she had heard him correctly.
+          </p>
+        </div>
+        <!-- tag rows (this will probably be a vue component) -->
+        <div class="flex flex-row flex-wrap gap-4 mt-4">
+          <span class="dark:text-primary px-2 py-1 rounded-full text-sm border-2 border-accent">Tag 1</span>
+          <span class="dark:text-primary px-2 py-1 rounded-full text-sm border-2 border-accent">Tag 2</span>
+          <span class="dark:text-primary px-2 py-1 rounded-full text-sm border-2 border-accent">Tag 3</span>
+        </div>
+
+        <div class="md:flex flex-row hidden mt-6 border-t-2 border-royal-blue-100">
+          <h3 class="dark:text-secondary font-semibold mt-2">Commit History Example</h3>
+        </div>
+        <div class="mt-4">
+          <span class="dark:text-secondary">Latest commit here!</span>
+        </div>
+      </div>
     </div>
   </div>
 
+  <div class="flex px-8 py-12">
+    <div class="flex-1 space-y-6">
+      <h1 class="dark:text-primary font-bold text-3xl">My Github Activity</h1>
+      <div class="group hover:-translate-y-1 bg-foreground backdrop-blur-sm p-6 rounded-2xl border
+          border-accent/20 transition-all duration-300 shadow-lg hover:shadow-accent/20">
+        <a href="https://www.github.com/saul178">
+          <img class="w-full" src="https://ghchart.rshah.org/072460/saul178.svg" alt="Github Contributations Chart">
+        </a>
+      </div>
+    </div>
+  </div>
 </template>
