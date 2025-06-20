1f2410aa6cefbf734cf308e61f561b2dbf2bc90f
Saul Gonzalez <gonzalezsaul31@gmail.com>
2025-06-19 21:32:02 -0600

    wip: func to fetch repo meta data is done for the most part, commits will be next

--- backend/handlers/github.go
+++ backend/handlers/github.go
@@ -1,13 +1,34 @@
 package handlers
 
 import (
+	"context"
 	"errors"
 	"log"
+	"net/http"
 	"os"
+	"time"
 
+	"github.com/gin-gonic/gin"
 	"github.com/google/go-github/v72/github"
 	"github.com/joho/godotenv"
 )
+
+type RepoMetadata struct {
+	Title     string   `json:"title,omitempty"`
+	Desc      string   `json:"desc,omitempty"`
+	Languages string   `json:"languages,omitempty"`
+	Commits   []string `json:"commits,omitempty"`
+	URL       string   `json:"url,omitempty"`
+}
+
+const (
+	githubUser    string = "saul178"
+	githubTimeout        = 5 * time.Second
+)
+
+func getPinnedRepos() []string {
+	return []string{"personal-website", "manga-library-proj", "Personal-SampleShare"}
+}
 
 func getGithubToken() (string, error) {
 	err := godotenv.Load()
@@ -30,4 +51,42 @@ func initGithubClient() *github.Client {
 
 	client := github.NewClient(nil).WithAuthToken(ghToken)
 	return client
+}
+
+func GetOwnerReposHandler(c *gin.Context) {
+	client := initGithubClient()
+
+	ctx, cancel := context.WithTimeout(context.Background(), githubTimeout)
+	defer cancel()
+
+	owner := githubUser
+	repos := getPinnedRepos()
+
+	var pinnedReposData []RepoMetadata
+	for i := range repos {
+		repository, resp, err := client.Repositories.Get(ctx, owner, repos[i])
+		if err != nil {
+			log.Printf("Error fetching repo %s: %v", repos[i], err)
+			continue
+		}
+		if resp.Response.StatusCode != http.StatusOK {
+			log.Printf("GET request for repo %s responded with %v", repos[i], resp.Response.StatusCode)
+			continue
+		}
+
+		metadata := RepoMetadata{
+			Title:     repository.GetName(),
+			Desc:      repository.GetDescription(),
+			Languages: repository.GetLanguage(),
+			URL:       repository.GetHTMLURL(),
+		}
+		pinnedReposData = append(pinnedReposData, metadata)
+	}
+
+	if len(pinnedReposData) == 0 {
+		log.Println("No repo data was fetched successfully")
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to fetch all repository data"})
+		return
+	}
+	c.JSON(http.StatusOK, pinnedReposData)
 }
