a9cd647ee2b45686be58a3b91539e8a679ed24f4
Saul Gonzalez <gonzalezsaul31@gmail.com>
2025-06-23 18:50:35 -0600

    wip: working on the json metadata

--- backend/handlers/home.go
+++ backend/handlers/home.go
@@ -1,1 +1,48 @@
 package handlers
+
+import (
+	"encoding/json"
+	"net/http"
+	"os"
+	"path"
+
+	"github.com/gin-gonic/gin"
+	m "github.com/saul178/personal-website/middleware"
+)
+
+type Skills struct {
+	OS         map[string]string `json:"os"`
+	Languages  map[string]string `json:"languages"`
+	Tools      map[string]string `json:"tools"`
+	Frameworks map[string]string `json:"frameworks"`
+	Databases  map[string]string `json:"databases"`
+}
+
+type Education struct {
+	School        string `json:"school"`
+	Degree        string `json:"degree"`
+	DateCompleted string `json:"dateCompleted"`
+}
+
+type Metadata struct {
+	Education []*Education `json:"education"`
+	Skills    *Skills      `json:"skills"`
+}
+
+func fetchHomeMetadata() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		filepath := path.Join("internal", "metadata", "data.json")
+		data, err := os.ReadFile(filepath)
+		if err != nil {
+			m.ErrorLog.Printf("Failed to read data from json file: %v", err)
+			c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to read data from metadata.json"})
+		}
+
+		var metadata *Metadata
+		if err := json.Unmarshal(data, &metadata); err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to parse json file"})
+		}
+
+		c.JSON(http.StatusOK, &metadata)
+	}
+}
