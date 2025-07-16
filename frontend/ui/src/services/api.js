d10b06a85f1fee2b00a11238a1cf6ac8a1aab6f3
Saul Gonzalez <gonzalezsaul31@gmail.com>
2025-07-15 19:24:33 -0600

    feat: added more security for the nginx.conf for the frontend webserver to deny requests from bots

--- frontend/ui/src/services/api.js
+++ frontend/ui/src/services/api.js
@@ -3,7 +3,7 @@ import axios from "axios";
 const axiosInstance = axios.create({
   // NOTE: change this back to /api when in prod, try to find a better way to switch between dev and
   // prod
-  baseURL: "http://localhost:8080/api",
+  baseURL: "/api",
 })
 
 export default axiosInstance
