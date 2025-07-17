4257ac010314ad1f969fa2f7b9bcb6e34105d40c
Saul Gonzalez <gonzalezsaul31@gmail.com>
2025-07-16 20:24:21 -0600

    fix: typo

--- backend/middleware/cors.go
+++ backend/middleware/cors.go
@@ -18,7 +18,7 @@ func CORSMiddleware(domain string) gin.HandlerFunc {
 		ctx.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
 		ctx.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
 
-		if ctx.Request.Method == "OPTIONS" || ctx.Request.Method == "HEADER" {
+		if ctx.Request.Method == "OPTIONS" || ctx.Request.Method == "HEAD" {
 			ctx.AbortWithStatus(http.StatusNoContent)
 			return
 		}
